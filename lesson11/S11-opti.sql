071518f645250d5f404306292e27189454f6342a
KholmanovAndrey Work <khav1985@gmail.com>
2019-11-12 12:52:46 +0700

    урок 11. Оптимизация запросов 1 задание

--- lesson11/S11-opti.sql
+++ lesson11/S11-opti.sql
@@ -1,9 +1,27 @@
 use shop;
 
+-- 1 задание
+drop table if exists logs;
 create table logs(
-	id SERIAL PRIMARY KEY,
 	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 	`table` VARCHAR(30),
 	element_id BIGINT UNSIGNED DEFAULT 0 NOT NULL,
 	name VARCHAR(254)
-) ;
+) ENGINE=Archive;
+
+INSERT INTO users (name)
+    VALUES('Никита');
+INSERT INTO logs (element_id, `table`, name)
+    VALUES(LAST_INSERT_ID(), 'users', 'Никита');
+       
+INSERT INTO catalogs (name)
+    VALUES('Жеский диск');
+INSERT INTO logs (element_id, `table`, name)
+    VALUES(LAST_INSERT_ID(), 'catalogs', 'Жеский диск');
+   
+INSERT INTO products (name)
+    VALUES('80ГБ');
+INSERT INTO logs (element_id, `table`, name)
+    VALUES(LAST_INSERT_ID(), 'products', '80ГБ');
+    
+-- 2 задание
